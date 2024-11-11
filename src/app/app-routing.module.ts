89c9e4a3c57b8c049f2cc6629dc7b561650d6bdd
MansourBalkhawmi <100216327+MansourBalkhawmi@users.noreply.github.com>
2024-11-11 14:01:41 +0000

    Reference et planning okk

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [{ path: 'ressources', loadChildren: () => import('./secure/ressources/ressources.module').then(m => m.RessourcesModule) },
   
-{ path: 'inscriptionReinscriptions', loadChildren: () => import('./secure/inscription-reinscriptions/inscription-reinscriptions.module').then(m => m.InscriptionReinscriptionsModule) }
+{ path: 'inscriptionReinscriptions', loadChildren: () => import('./secure/inscription-reinscriptions/inscription-reinscriptions.module').then(m => m.InscriptionReinscriptionsModule) },
+  
+{ path: 'referentiels', loadChildren: () => import('./secure/referentiels/referentiels.module').then(m => m.ReferentielsModule) },
+  
+{ path: 'planning', loadChildren: () => import('./secure/planning/planning.module').then(m => m.PlanningModule) }
   
 ];
 
